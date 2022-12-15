ee2eca81adb132f69d3b22ceb3a99c11c7fc9285
JoaoVitorAR <jvalves1998@outlook.com.br>
2022-12-15 15:08:37 -0300

    Finalização das rotas http

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,12 +1,24 @@
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { CancelNotification } from './../../application/use-cases/cancel-notification';
 import { Module } from "@nestjs/common";
 
 import { DatabaseModule } from './../database/database.module';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { NotificationsController } from './controller/notifications.controller';
+import { ReadNotification } from '@application/use-cases/read-notifications';
+import { GetRecipientNotification } from '@application/use-cases/get-recipient-notifications';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notifications';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationsController],
-    providers: [SendNotification] 
+    providers: [
+        SendNotification,
+        CancelNotification,
+        ReadNotification,
+        UnreadNotification,
+        GetRecipientNotification,
+        CountRecipientNotification
+    ] 
 })
 export class HttpModule {}
